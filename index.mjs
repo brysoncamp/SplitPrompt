1a239cd953e9e103f9664d78861564adb4ab5ae1
Bryson Camp <brysoncamp152@gmail.com>
2023-08-29 19:52:37 +1000

    Initial commit

---
+++ index.mjs
@@ -0,0 +1,17 @@
+const colorPreference = window.matchMedia("(prefers-color-scheme: dark)");
+
+colorPreference.addEventListener("change", (event) => {
+  const theme = event.matches ? "dark" : "light";
+  document.documentElement.dataset.theme = theme;
+  localStorage.removeItem("theme");
+});
+
+const themeToggle = document.getElementById("theme-toggle");
+
+themeToggle.addEventListener("click", () => {
+  const currentTheme = document.documentElement.dataset.theme;
+  const newTheme = currentTheme === "light" ? "dark" : "light";
+  document.documentElement.dataset.theme = newTheme;
+  localStorage.setItem("theme", newTheme);
+});
+
